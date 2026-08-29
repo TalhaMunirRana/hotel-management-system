9ec6fbf8d89c94adf513f951f7eb176916737955
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-29 22:45:54 +0500

    Added basic skeleton of room class

--- main.py
+++ main.py
@@ -37,10 +37,38 @@ class Hotel():
 
 class Room():
     """Contains the implementation of room"""
-    def __init__(self, room_number, guest):
+    def __init__(self, room_number, room_type, price, availability, cleanliness, maintenance_status):
         """Initialize the parameters"""
         self.room_number = room_number
-        self.guest = guest
+        self.room_type = room_type
+        self.price = price
+        self.availability = availability
+        self.cleanliness = cleanliness
+        self.maintenance_status = maintenance_status
+
+    def check_availability(self):
+        """Checks the rooms availability"""
+        ...
+
+    def updated_status(self):
+        """Updates the current status of the room"""
+        ...
+
+    def mark_clean(self):
+        """Marks the room as cleaned"""
+        ...
+
+    def mark_dirty(self):
+        """Marks the room as dirty."""
+        ...
+
+    def put_under_maintainance(self):
+        """Change the maintenance status of the room as under maintenance"""
+        ...
+
+    def remove_from_maintainance(self):
+        """Change the maintenance status of the room as maintained"""
+        ...
 
 class Guest():
     """Handles the guest information"""
