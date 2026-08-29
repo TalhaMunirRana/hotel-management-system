92a38d70b7a0b4c263ed6f18f127beff442e6b0b
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-29 22:37:04 +0500

    Added basic skeleton of hotel class

--- main.py
+++ main.py
@@ -5,7 +5,35 @@ class Hotel():
         self.name = name
         self.rooms = []
         self.guests = []
+        self.reservations = []
+    
+    def add_room(self):
+        """Adds the room in the hotel management"""
+        ...
 
+    def register_guest(self):
+        """Registers the guest in the hotel's system"""
+        ...
+
+    def make_reservation(self):
+        """Books the reservation for the guest"""
+        ...
+
+    def cancel_reservation(self):
+        """Cancels the reservation and make the room available again"""
+        ...
+
+    def show_available_rooms(self):
+        """Lists all the available rooms in the hotel."""
+        ...
+
+    def show_guests(self):
+        """Show all the guests with their information reservation"""
+        ...
+
+    def find_reservation(self):
+        """Find the specific reservation and it's information"""
+        ...
 
 class Room():
     """Contains the implementation of room"""
@@ -27,13 +55,5 @@ class Reservation():
     """Handles the reservation details"""
     def __init__(self, guest, room):
         """Initialize the parameters"""
-        self.guest = guest
-        self.room = room
-
-class Payment():
-    """Handles the payment processng"""
-    def __init__(self, reservation, guest, room):
-        """Initialize the parameters"""
-        self.reservation = reservation
         self.guest = guest
         self.room = room
